2636a92db2b628423c083c2a15b0e076ef3ab2c4
Mario vg <22670080@itiguala.edu.mx>
2023-02-15 12:19:13 -0600

    Animal veterinaria

---
+++ src/Animal.java
@@ -0,0 +1,28 @@
+public class Animal {
+    private String specie;
+
+    public String getSpecie(){
+        return specie;
+    }
+    public  void setSpecie(String specie){
+        this.specie = specie;
+    }
+
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public  void setName(String name){
+        this.name = name;
+    }
+
+    private Integer age ;
+
+    public Integer getAge(){
+        return age;
+    }
+    public  void setAge(Integer age){
+        this.age = age;
+    }
+}
